947e7d4da2588ee711fb46663b13caf7b5f730e4
ttaleh <taleh958@gmail.com>
2026-03-29 00:22:56 +0200

    build: suppress typeorm optional driver warnings in webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize typeorm on server side to avoid bundling optional drivers
+      config.externals = config.externals || [];
+      config.externals.push({
+        'typeorm': 'typeorm',
+        'typeorm/globals': 'typeorm/globals',
+        'typeorm/driver/DriverFactory': 'typeorm/driver/DriverFactory',
+      });
+
+      // Suppress warnings for optional typeorm drivers not included
+      config.ignoreWarnings = config.ignoreWarnings || [];
+      config.ignoreWarnings.push({
+        module: /node_modules\/typeorm/,
+        message: /Can't resolve.*(?:react-native-sqlite-storage|@sap\/hana-client|mysql|oracle|pg)/,
+      });
+      config.ignoreWarnings.push({
+        module: /node_modules\/typeorm/,
+        message: /Critical dependency: the request of a dependency is an expression/,
+      });
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
